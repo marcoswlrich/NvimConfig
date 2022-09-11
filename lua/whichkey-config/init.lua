0a54254b48935c3097aa72fac23ed8414e8f7051
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-10 21:01:34 -0400

    Whichkey-config

---
+++ lua/whichkey-config/init.lua
@@ -0,0 +1,43 @@
+local wk = require("which-key")
+
+wk.setup {
+  plugins = {
+    marks = false,
+    registers = false,
+    spelling = {enabled = false, suggestions = 20},
+    presets = {
+      operators = false,
+      motions = false,
+      text_objects = false,
+      windows = false,
+      nav = false,
+      z = false,
+      g = false
+    }
+  }
+}
+
+local Terminal = require('toggleterm.terminal').Terminal
+local toggle_float = function()
+  local float = Terminal:new({direction = "float"})
+  return float:toggle()
+end
+local toggle_lazygit = function()
+  local lazygit = Terminal:new({cmd = 'lazygit', direction = "float"})
+  return lazygit:toggle()
+end
+local mappings = {
+  q = {":q<cr>", "Quit"},
+  Q = {":wq<cr>", "Save & Quit"},
+  w = {":w<cr>", "Save"},
+  x = {":bdelete<cr>", "Close"},
+  E = {":e ~/.config/nvim/init.lua<cr>", "Edit config"},
+  s = {
+    t = {":ToggleTerm<cr>", "Split Below"},
+    f = {toggle_float, "Floating Terminal"},
+    l = {toggle_lazygit, "LazyGit"}
+  },
+}
+
+local opts = {prefix = '<leader>'}
+wk.register(mappings, opts)

8e3325ed7b9a8901438711aca1e4cdd7fdf5026e
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-10 21:03:36 -0400

    News Config global

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -3,8 +3,11 @@ local set = vim.opt
 
 set.expandtab = true
 set.smarttab = true
-set.shiftwidth = 4
-set.tabstop = 4
+set.shiftwidth = 2
+set.tabstop = 2
+
+set.termguicolors = true
+set.showmode = false
 
 set.hlsearch = true
 set.incsearch = true
@@ -14,17 +17,20 @@ set.smartcase = true
 set.splitbelow = true
 set.splitright = true
 set.wrap = false
---set.breakindent = true
+set.breakindent = true
 set.scrolloff = 5
 set.fileencoding = "utf-8"
+set.encoding = 'utf-8'
 set.conceallevel = 2
 
 set.relativenumber = true
 set.cursorline = true
 set.wildmenu = true
---set.completeopt = "menuone,noselect"
+set.completeopt = "menuone,noselect"
 
 set.hidden = true
 set.mouse = "a"
 
 set.fillchars = "eob: "
+
+vim.notify = require("notify")

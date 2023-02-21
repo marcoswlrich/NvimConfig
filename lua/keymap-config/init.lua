baa5a4c31fe7936af929d9dab40dcf44a7af1cdb
marcoswlrich <marcosapw.dev@gmail.com>
2023-02-20 23:33:10 -0400

    Update

--- lua/keymap-config/init.lua
+++ lua/keymap-config/init.lua
@@ -1,28 +1,24 @@
-local map = vim.api.nvim_set_keymap
+M = {}
 local opts = { noremap = true, silent = true }
 
-vim.g.mapleader = " " -- leader key
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
 
---> nvim tree mappings <--
-map("n", "<leader>t", ":NvimTreeToggle<CR>", opts)
-map("n", "<leader>gt", ":NvimTreeFocus<CR>", opts)
--- Barbar--- 
-map("n", "<A-,>", ":BufferPrevious<CR>", opts)
-map("n", "<A-.>", ":BufferNext<CR>", opts)
-map("n", "<A-<>", ":BufferMovePrevious<CR>", opts)
-map("n", "<A->>", ":BufferMoveNext<CR>", opts)
-map("n", "<A-1>", ":BufferGoto 1<CR>", opts)
-map("n", "<A-2>", ":BufferGoto 2<CR>", opts)
-map("n", "<A-3>", ":BufferGoto 3<CR>", opts)
-map("n", "<A-4>", ":BufferGoto 4<CR>", opts)
-map("n", "<A-5>", ":BufferGoto 5<CR>", opts)
-map("n", "<A-6>", ":BufferGoto 6<CR>", opts)
-map("n", "<A-7>", ":BufferGoto 7<CR>", opts)
-map("n", "<A-8>", ":BufferGoto 8<CR>", opts)
-map("n", "<A-9>", ":BufferGoto 9<CR>", opts)
-map("n", "<A-0>", ":BufferLast<CR>", opts)
-map("n", "<A-c>", ":BufferClose<CR>", opts)
-map("n", "<C-p>", ":BufferPick<CR>", opts)
-map("n", "<leader>bb", ":BufferOrderByBufferNumber<CR>", opts)
-map("n", "<leader>bd", ":BufferOrderByDirectory<CR>", opts)
-map("n", "<leader>bl", ":BufferOrderByLanguage<CR>", opts)
+---------------------
+-- General Keymaps
+---------------------
+
+keymap("n", "<Space>", "", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
+keymap("n", "<C-i>", "<C-i>", opts)
+
+----------------------
+-- Plugin Keybinds
+----------------------
+
+keymap("n", "<leader>e", '<cmd>NvimTreeToggle<cr>', opts)
+
+--telescope
+

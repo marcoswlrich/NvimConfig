3944b17676c794841a59f168ef215f54722c995f
marcoswlrich <marcosapw.dev@gmail.com>
2023-02-20 23:36:19 -0400

    Update

--- lua/autopairs-config/init.lua
+++ lua/autopairs-config/init.lua
@@ -1,4 +1,6 @@
-require('nvim-autopairs').setup({check_ts = true})
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
+local keymap = vim.keymap
+
+keymap.set('n', '<space>ta', '<cmd>ToggleAlternate<cr>')
+require('nvim-autopairs').setup({
+  disable_filetype = { 'TelescopePrompt', 'vim' }
+})

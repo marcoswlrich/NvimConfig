93df83825cb33959eb15007e2a71c81135f5e52e
marcoswlrich <marcosapw.dev@gmail.com>
2023-04-01 00:26:20 -0400

    New Keymaps

--- lua/keymap-config/init.lua
+++ lua/keymap-config/init.lua
@@ -11,14 +11,21 @@ local keymap = vim.api.nvim_set_keymap
 keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
-keymap("n", "<C-i>", "<C-i>", opts)
+
+-- WINDOWS
+keymap("n", "<leader>sv", "<C-w>v", opts) -- abrir janela vertical
+keymap("n", "<leader>sh", "<C-w>s", opts) -- abri janela horizontal
+keymap("n", "<leader>se", "<C-w>=", opts) -- deixar janelas abertas com mesmo tamanho
+keymap("n", "<leader>sx", ":close<CR>", opts) -- close split window
+--keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
 
 ----------------------
 -- Plugin Keybinds
 ----------------------
 
 keymap("n", "<leader>e", '<cmd>NvimTreeToggle<cr>', opts)
+-- Terminal
+keymap("n", "<leader>th", "<CMD>ToggleTerm size=10 direction=horizontal<CR>", opts)
+keymap("n", "<leader>tv", "<CMD>ToggleTerm size=80 direction=vertical<CR>", opts)
 
 --telescope
-

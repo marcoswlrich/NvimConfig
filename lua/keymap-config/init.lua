9416a2b956ace5e431fcf1a940d1e48d9f933c93
marcoswlrich <marcosapw.dev@gmail.com>
2023-04-01 02:20:45 -0400

    New Keymaps

--- lua/keymap-config/init.lua
+++ lua/keymap-config/init.lua
@@ -12,12 +12,38 @@ keymap("n", "<Space>", "", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+keymap("i", "jk", "<ESC>", opts)
+
 -- WINDOWS
 keymap("n", "<leader>sv", "<C-w>v", opts) -- abrir janela vertical
 keymap("n", "<leader>sh", "<C-w>s", opts) -- abri janela horizontal
 keymap("n", "<leader>se", "<C-w>=", opts) -- deixar janelas abertas com mesmo tamanho
 keymap("n", "<leader>sx", ":close<CR>", opts) -- close split window
 --keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
+
+------------------
+--VisuaL Mode
+------------------
+keymap('v', '<c-a>', 'gg<S-v>G', opts)
+
+-- Stay in indent mode
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
+
+-- Move text up and down
+keymap('v', '<A-j>', ':m .+1<CR>==', opts)
+keymap('v', '<A-k>', ':m .-2<CR>==', opts)
+
+-- Paste
+keymap('v', 'p', '"_dP', opts)
+keymap('x', '<leader>pp', '"_dP', opts)
+
+-- Move text up and down
+keymap('x', 'J', ":move '>+1<CR>gv-gv", opts)
+keymap('x', 'K', ":move '<-2<CR>gv-gv", opts)
+keymap('x', '<A-j>', ":move '>+1<CR>gv-gv", opts)
+keymap('x', '<A-k>', ":move '<-2<CR>gv-gv", opts)
+
 
 ----------------------
 -- Plugin Keybinds

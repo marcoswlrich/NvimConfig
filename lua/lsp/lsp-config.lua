e424f3f4af7e31f70453b18b6707d6b94edd096d
marcoswlrich <marcosapw.dev@gmail.com>
2023-04-04 01:32:29 -0400

    Update config

--- lua/lsp/lsp-config.lua
+++ lua/lsp/lsp-config.lua
@@ -1,73 +1,70 @@
 local on_attach = function(_, bufnr)
-	-- format on save
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		buffer = bufnr,
-		callback = function()
-			vim.lsp.buf.format()
-		end,
-	})
+  -- format on save
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format()
+    end,
+  })
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lsp_config = {
-	capabilities = capabilities,
-	group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
-	on_attach = function(_, bufnr)
-		on_attach(_, bufnr)
-	end,
+  capabilities = capabilities,
+  group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+  on_attach = function(_, bufnr)
+    on_attach(_, bufnr)
+  end,
 }
 
 require("mason-lspconfig").setup_handlers({
-	function(server_name)
-		require("lspconfig")[server_name].setup(lsp_config)
-	end,
-	lua_ls = function()
-		require("lspconfig").lua_ls.setup(vim.tbl_extend("force", lsp_config, {
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		}))
-	end,
-	tsserver = function()
-		require("typescript").setup({
-			server = vim.tbl_extend("force", lsp_config, {
-				on_attach = function(_, bufnr)
-					on_attach(_, bufnr)
-				end,
-				init_options = {
-					preferences = {
-						importModuleSpecifierPreference = "project=relative",
-						jsxAttributeCompletionStyle = "none",
-					},
-				},
-			}),
-		})
-	end,
-	pyright = function()
-		require("lspconfig").pyright.setup(vim.tbl_extend("force", lsp_config, {
-			on_attach = function(_, bufnr)
-				on_attach(_, bufnr)
-			end,
-			cmd = { "pyright-langserver", "--stdio" },
-			filetypes = { "python" },
-			settings = {
-				python = {
-					analysis = {
-						autoSearchPaths = true,
-						diagnosticMode = "workspace",
-						useLibraryCodeForTypes = true,
-					},
-				},
-			},
-			single_file_suport = true,
-		}))
-	end,
+  function(server_name)
+    require("lspconfig")[server_name].setup(lsp_config)
+  end,
+  lua_ls = function()
+    require("lspconfig").lua_ls.setup(vim.tbl_extend("force", lsp_config, {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    }))
+  end,
+  tsserver = function()
+    require("typescript").setup({
+      server = vim.tbl_extend("force", lsp_config, {
+        on_attach = function(_, bufnr)
+          on_attach(_, bufnr)
+        end,
+        init_options = {
+          preferences = {
+            importModuleSpecifierPreference = "project=relative",
+            jsxAttributeCompletionStyle = "none",
+          },
+        },
+      }),
+    })
+  end,
+  pyright = function()
+    require("lspconfig").pyright.setup(vim.tbl_extend("force", lsp_config, {
+      on_attach = function(_, bufnr)
+        on_attach(_, bufnr)
+      end,
+      cmd = { "pyright-langserver", "--stdio" },
+      filetypes = { "python" },
+      settings = {
+        python = {
+          analysis = {
+            autoSearchPaths = true,
+            diagnosticMode = "workspace",
+            useLibraryCodeForTypes = true,
+          },
+        },
+      },
+      single_file_suport = true,
+    }))
+  end,
 })
-vim.keymap.set("n", "<leader>i", "<cmd>TypescriptOrganizeImports<cr>")
-vim.keymap.set("n", "<leader>ax", "<cmd>TypescriptAddMissingImports<cr>")
-vim.keymap.set("n", "<leader>r", "<cmd>TypescriptRemoveUnused<cr>")

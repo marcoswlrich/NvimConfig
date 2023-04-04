03062aef50901c553fd6848de5367253863325b5
marcoswlrich <marcosapw.dev@gmail.com>
2023-04-04 01:31:37 -0400

    Update config

--- lua/cmp-config/lspkind.lua
+++ lua/cmp-config/lspkind.lua
@@ -1,6 +1,20 @@
-require('lspkind').init({
-  mode = 'symbol_text',
+local status, lspkind = pcall(require, "lspkind")
+if (not status) then return end
+
+lspkind.init({
+  -- enables text annotations
+  --
+  -- default: true
+  mode = 'symbol',
+  -- default symbol map
+  -- can be either 'default' (requires nerd-fonts font) or
+  -- 'codicons' for codicon preset (requires vscode-codicons font)
+  --
+  -- default: 'default'
   preset = 'codicons',
+  -- override preset symbols
+  --
+  -- default: {}
   symbol_map = {
     Text = "",
     Method = "",
@@ -14,7 +28,7 @@ require('lspkind').init({
     Property = "ﰠ",
     Unit = "塞",
     Value = "",
-    Enum = "",
+    Enum = "",
     Keyword = "",
     Snippet = "",
     Color = "",
@@ -26,6 +40,6 @@ require('lspkind').init({
     Struct = "פּ",
     Event = "",
     Operator = "",
-    TypeParameter = ""
+    TypeParameter = ""
   },
 })

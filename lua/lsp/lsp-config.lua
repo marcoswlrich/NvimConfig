890f9f21341cf9e650f69035f043744aa3948c13
marcoswlrich <marcosapw.dev@gmail.com>
2023-04-05 22:19:48 -0400

    Tailwind config server

--- lua/lsp/lsp-config.lua
+++ lua/lsp/lsp-config.lua
@@ -86,5 +86,24 @@ require("mason-lspconfig").setup_handlers({
         },
       },
     }))
+  end,
+  tailwindcss = function()
+    require('lspconfig').tailwindcss.setup(vim.tbl_extend("force", lsp_config, {
+      on_attach = function(_, bufnr)
+        on_attach(_, bufnr)
+      end,
+      cmp = { "tailwindcss-language-server", "--stdio" },
+      filetypes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "clojure", "django-html", "htmldjango",
+        "edge", "eelixir", "elixir", "ejs", "erb", "eruby", "gohtml", "haml", "handlebars", "hbs", "html", "html-eex",
+        "heex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim",
+        "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss", "javascript", "javascriptreact", "reason",
+        "rescript", "typescript", "typescriptreact", "vue", "svelte" },
+      init_options = {
+        userLanguages = {
+          eelixir = "html-eex",
+          eruby = "erb",
+        },
+      },
+    }))
   end
 })

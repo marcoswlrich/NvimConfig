c892fee8beaeb2607258db75c5932d94af284191
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-04 04:32:13 -0400

    Correcoes Bugs Update Servers

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -1,6 +1,11 @@
 require("mason").setup({
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4,
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
 })
 require("mason-lspconfig").setup({
   ensure_installed = {

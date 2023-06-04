3def70c51330c091aa8c3508b83413e7ef273ce0
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-04 04:30:13 -0400

    Config RosePine e transparencia themes

--- lua/theme-config/init.lua
+++ lua/theme-config/init.lua
@@ -1,9 +1,9 @@
 require("tokyonight").setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
-  style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  light_style = "day", -- The theme is used when the background is set to light
-  transparent = false, -- Enable this to disable setting the background color
+  style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  light_style = "day",    -- The theme is used when the background is set to light
+  transparent = true,     -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
     -- Style to be applied to different syntax groups
@@ -13,14 +13,14 @@ require("tokyonight").setup({
     functions = {},
     variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
-    sidebars = "dark", -- style for sidebars, see below
-    floats = "dark", -- style for floating windows
+    sidebars = "dark",              -- style for sidebars, see below
+    floats = "dark",                -- style for floating windows
   },
-  sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-  dim_inactive = false, -- dims inactive windows
-  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+  dim_inactive = false,             -- dims inactive windows
+  lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
   --- You can override specific color groups to use other groups or a hex color
   --- function will be called with a ColorScheme table
   --- #@param colors ColorScheme
@@ -36,10 +36,10 @@ require("tokyonight").setup({
 
 require("catppuccin").setup({
   flavour = "mocha", -- latte, frappe, macchiato, mocha
-  background = { -- :h background
+  background = {     -- :h background
     dark = "mocha",
   },
-  transparent_background = false,
+  transparent_background = true,
   show_end_of_buffer = false, -- show the '~' characters after the end of buffers
   term_colors = false,
   dim_inactive = {
@@ -48,7 +48,7 @@ require("catppuccin").setup({
     percentage = 0.15,
   },
   no_italic = false, -- Force no italic
-  no_bold = false, -- Force no bold
+  no_bold = false,   -- Force no bold
   styles = {
     comments = { "italic" },
     conditionals = { "italic" },
@@ -74,4 +74,54 @@ require("catppuccin").setup({
     mini = false,
     -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   },
+})
+
+require('rose-pine').setup({
+  --- @usage 'auto'|'main'|'moon'|'dawn'
+  variant = 'auto',
+  --- @usage 'main'|'moon'|'dawn'
+  dark_variant = 'moon',
+  bold_vert_split = false,
+  dim_nc_background = false,
+  disable_background = false,
+  disable_float_background = false,
+  disable_italics = false,
+
+  --- @usage string hex value or named color from rosepinetheme.com/palette
+  groups = {
+    background = 'base',
+    background_nc = '_experimental_nc',
+    panel = 'surface',
+    panel_nc = 'base',
+    border = 'highlight_med',
+    comment = 'muted',
+    link = 'iris',
+    punctuation = 'subtle',
+
+    error = 'love',
+    hint = 'iris',
+    info = 'foam',
+    warn = 'gold',
+
+    headings = {
+      h1 = 'iris',
+      h2 = 'foam',
+      h3 = 'rose',
+      h4 = 'gold',
+      h5 = 'pine',
+      h6 = 'foam',
+    }
+    -- or set all headings at once
+    -- headings = 'subtle'
+  },
+
+  -- Change specific vim highlight groups
+  -- https://github.com/rose-pine/neovim/wiki/Recipes
+  highlight_groups = {
+    ColorColumn = { bg = 'rose' },
+
+    -- Blend colours against the "base" background
+    CursorLine = { bg = 'foam', blend = 10 },
+    StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+  }
 })

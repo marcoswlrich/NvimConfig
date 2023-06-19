8ed4f03e5ef5d1e31148e1ae9686b3a94c5f59c0
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-19 02:31:30 -0400

    Update Config

--- lua/dap-config/init.lua
+++ lua/dap-config/init.lua
@@ -8,6 +8,91 @@ if not dap_ui_status_ok then
   return
 end
 
+local virtual_text = require("nvim-dap-virtual-text")
+local dap_go = require("dap-go")
+
+dap.adapters.python = function(cb, config)
+  if config.request == 'attach' then
+    ---@diagnostic disable-next-line: undefined-field
+    local port = (config.connect or config).port
+    ---@diagnostic disable-next-line: undefined-field
+    local host = (config.connect or config).host or '127.0.0.1'
+    cb({
+      type = 'server',
+      port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+      host = host,
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  else
+    cb({
+      type = 'executable',
+      command = '/marcoswlrich/.virtualenvs/debugpy/bin',
+      args = { '-m', 'debugpy.adapter' },
+      options = {
+        source_filetype = 'python',
+      },
+    })
+  end
+end
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch',
+    name = "Launch file",
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}", -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return '/usr/bin/python'
+      end
+    end,
+  },
+}
+
+dap.adapters.lldb = {
+  type = 'executable',
+  command = '/usr/bin/lldb-vscode',
+  name = 'lldb'
+}
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+local codelldb_adapter = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = mason_path .. "bin/codelldb",
+    args = { "--port", "${port}" },
+  },
+}
+dap.adapters.codelldb = codelldb_adapter
+local lldb = {
+  name = 'Launch',
+  type = 'codelldb',
+  request = 'launch',
+  program = function()
+    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  end,
+  cwd = '${workspaceFolder}',
+  stopOnEntry = false,
+  args = {},
+}
+
+dap.configurations.rust = { lldb }
+dap.configurations.c = { lldb }
+
 -- dapui.setup()
 dapui.setup {
   icons = { expanded = "▾", collapsed = "▸" },
@@ -52,8 +137,8 @@ dapui.setup {
     },
   },
   floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
+    max_height = nil,  -- These can be integers or a float between 0 and 1.
+    max_width = nil,   -- Floats will be treated as percentage of your screen.
     border = "single", -- Border style. Can be "single", "double" or "rounded"
     mappings = {
       close = { "q", "<Esc>" },
@@ -76,3 +161,6 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close {}
 end
+
+virtual_text.setup()
+dap_go.setup()

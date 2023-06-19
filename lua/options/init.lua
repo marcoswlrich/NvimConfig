07e25a1205c25aefec0d329cd91fa0de8b6f4ab3
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-18 22:19:24 -0400

    News options

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -11,7 +11,7 @@ opt.incsearch = true
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
 opt.tabstop = 2
@@ -25,7 +25,7 @@ opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 opt.smartindent = true
 
 -- cursor line
@@ -35,7 +35,7 @@ opt.cursorline = true -- highlight the current cursor line
 opt.termguicolors = true
 opt.showmode = false
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -44,8 +44,8 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true     -- split vertical window to the right
+opt.splitbelow = true     -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
 opt.mouse = "a"
@@ -56,3 +56,19 @@ opt.numberwidth = 4
 opt.guifont = "monospace:h17"
 
 opt.completeopt = "menuone,noselect"
+opt.conceallevel = 0
+opt.showtabline = 0
+opt.swapfile = false
+opt.undofile = true
+opt.updatetime = 100
+opt.breakindent = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.laststatus = 3
+opt.showcmd = false
+opt.title = true
+opt.confirm = true
+opt.fillchars = { eob = " " }
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '

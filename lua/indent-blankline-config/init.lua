f590f8886c4fa2b04c76676a9cc2337b61168bb4
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-19 02:28:06 -0400

    Indent

---
+++ lua/indent-blankline-config/init.lua
@@ -0,0 +1,7 @@
+require("indent_blankline").setup {
+  char = "│",
+  filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble" },
+  show_end_of_line = true,
+  show_trailing_blankline_indent = false,
+  show_current_context = false,
+}

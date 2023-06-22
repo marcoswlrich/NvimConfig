090033e4ef5889b389bb23e6815e6cc97a7a3958
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-22 02:11:05 -0400

    Glance plugin

--- lua/glance-config/init.lua
+++ lua/glance-config/init.lua
@@ -2,25 +2,6 @@ local glance = require("glance")
 --local actions = glance.actions
 
 glance.setup({
-	height = 18, -- Height of the window
-	zindex = 45,
-
-	-- By default glance will open preview "embedded" within your active window
-	-- when `detached` is enabled, glance will render above all existing windows
-	-- and won't be restiricted by the width of your active window
-	detached = true,
-
-	-- Or use a function to enable `detached` only when the active window is too small
-	-- (default behavior)
-	detached = function(winid)
-		return vim.api.nvim_win_get_width(winid) < 100
-	end,
-
-	preview_win_opts = { -- Configure preview window options
-		cursorline = true,
-		number = true,
-		wrap = true,
-	},
 	border = {
 		enable = false, -- Show window borders. Only horizontal borders allowed
 		top_char = "―",
@@ -48,9 +29,3 @@ glance.setup({
 		enable = true, -- Available strating from nvim-0.8+
 	},
 })
-
---keys = {
---{ "gD", "<CMD>Glance definitions<CR>",      desc = "Glance definitions" },
---{ "gR", "<CMD>Glance references<CR>",       desc = "Glance references" },
---{ "gY", "<CMD>Glance type_definitions<CR>", desc = "Glance type_definitions" },
---{ "gM", "<CMD>Glance implementations<CR>",  desc = "Glance implementations" }

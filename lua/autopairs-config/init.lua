9dbc1706090f10672ed3656758441aa4ac0c7817
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-29 20:24:08 -0400

    Update config

--- lua/autopairs-config/init.lua
+++ lua/autopairs-config/init.lua
@@ -1,6 +1,3 @@
-local keymap = vim.keymap
-
-keymap.set('n', '<space>ta', '<cmd>ToggleAlternate<cr>')
-require('nvim-autopairs').setup({
-  disable_filetype = { 'TelescopePrompt', 'vim' }
+require("nvim-autopairs").setup({
+	disable_filetype = { "TelescopePrompt", "vim" },
 })

b61323b90b0c2a178bbf95f4653e1ff3414a547e
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-29 20:41:17 -0400

    New config icons

--- lua/cmp-config/cmp.lua
+++ lua/cmp-config/cmp.lua
@@ -2,6 +2,24 @@ local cmp = require("cmp")
 local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
+local function formatForTailwindCSS(entry, vim_item)
+	if vim_item.kind == "Color" and entry.completion_item.documentation then
+		local _, _, r, g, b = string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
+		if r then
+			local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
+			local group = "Tw_" .. color
+			if vim.fn.hlID(group) < 1 then
+				vim.api.nvim_set_hl(0, group, { fg = "#" .. color })
+			end
+			vim_item.kind = "●"
+			vim_item.kind_hl_group = group
+			return vim_item
+		end
+	end
+	vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+	return vim_item
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -41,17 +59,13 @@ cmp.setup({
 		{ name = "buffer" },
 	}),
 	formatting = {
-		format = function(entry, vim_item)
-			if vim.tbl_contains({ "path" }, entry.source.name) then
-				local icon, hl_group = require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
-				if icon then
-					vim_item.kind = icon
-					vim_item.kind_hl_group = hl_group
-					return vim_item
-				end
-			end
-			return lspkind.cmp_format({ with_text = true })(entry, vim_item)
-		end,
+		format = lspkind.cmp_format({
+			maxwidth = 50,
+			before = function(entry, vim_item)
+				vim_item = formatForTailwindCSS(entry, vim_item)
+				return vim_item
+			end,
+		}),
 	},
 })
 

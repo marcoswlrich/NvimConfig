f8b3f07a0736687a422c62df02b83db05e12b3ba
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-29 20:40:35 -0400

    Update config theme tokyo night

--- lua/scrollbar-config/init.lua
+++ lua/scrollbar-config/init.lua
@@ -1,123 +1,15 @@
+local colors = require("tokyonight.colors").setup()
+
 require("scrollbar").setup({
-	show = true,
-	show_in_active_only = false,
-	set_highlights = true,
-	folds = 1000, -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
-	max_lines = false, -- disables if no. of lines in buffer exceeds this
 	handle = {
-		text = " ",
-		color = "#191724",
-		cterm = nil,
-		-- highlight = "CursorColumn",
-		hide_if_all_visible = false, -- Hides handle if all lines are visible
+		color = colors.bg_highlight,
 	},
 	marks = {
-		Cursor = {
-			-- text = "•",
-			text = "─",
-			priority = 0,
-			color = "#C4A7E7",
-			cterm = nil,
-			-- highlight = "Normal",
-		},
-		Search = {
-			text = { "-", "=" },
-			priority = 1,
-			color = "#F6C177",
-			cterm = nil,
-			highlight = "Search",
-		},
-		Error = {
-			text = { "-", "=" },
-			priority = 2,
-			color = "#EB6F92",
-			cterm = nil,
-			highlight = "DiagnosticVirtualTextError",
-		},
-		Warn = {
-			text = { "-", "=" },
-			priority = 3,
-			color = nil,
-			cterm = nil,
-			highlight = "DiagnosticVirtualTextWarn",
-		},
-		Info = {
-			text = { "-", "=" },
-			priority = 4,
-			color = nil,
-			cterm = nil,
-			highlight = "DiagnosticVirtualTextInfo",
-		},
-		Hint = {
-			text = { "-", "=" },
-			priority = 5,
-			color = nil,
-			cterm = nil,
-			highlight = "DiagnosticVirtualTextHint",
-		},
-		Misc = {
-			text = { "-", "=" },
-			priority = 6,
-			color = nil,
-			cterm = nil,
-			highlight = "Normal",
-		},
-		GitAdd = {
-			text = "┆",
-			priority = 7,
-			color = "#9CCFD8",
-			cterm = nil,
-			highlight = "GitSignsAdd",
-		},
-		GitChange = {
-			text = "┆",
-			priority = 7,
-			color = nil,
-			cterm = nil,
-			highlight = "GitSignsChange",
-		},
-		GitDelete = {
-			text = "▁",
-			priority = 7,
-			color = nil,
-			cterm = nil,
-			highlight = "GitSignsDelete",
-		},
-	},
-	excluded_buftypes = {
-		"terminal",
-	},
-	excluded_filetypes = {
-		"prompt",
-		"TelescopePrompt",
-		"noice",
-		"alpha",
-		"NvimTree",
-		"",
-	},
-	autocmd = {
-		render = {
-			"BufWinEnter",
-			"TabEnter",
-			"TermEnter",
-			"WinEnter",
-			"CmdwinLeave",
-			"TextChanged",
-			"VimResized",
-			"WinScrolled",
-		},
-		clear = {
-			"BufWinLeave",
-			"TabLeave",
-			"TermLeave",
-			"WinLeave",
-		},
-	},
-	handlers = {
-		cursor = true,
-		diagnostic = true,
-		gitsigns = false, -- Requires gitsigns
-		handle = true,
-		search = false, -- Requires hlslens
+		Search = { color = colors.orange },
+		Error = { color = colors.error },
+		Warn = { color = colors.warning },
+		Info = { color = colors.info },
+		Hint = { color = colors.hint },
+		Misc = { color = colors.purple },
 	},
 })

9dcd2080d8108a871474fe351a52ac77789e648e
marcoswlrich <marcosapw.dev@gmail.com>
2023-09-12 00:19:55 -0400

    Config GitBlames

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -73,7 +73,7 @@ opt.fillchars = { eob = " " }
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.g.gitblame_enabled = 0
+vim.g.gitblame_enabled = 1
 vim.g.gitblame_message_template = "<summary> • <date> • <author>"
 vim.g.gitblame_highlight_group = "LineNr"
 

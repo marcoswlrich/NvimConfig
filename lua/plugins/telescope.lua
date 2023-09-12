8f9e1385af76d9498f99328e536c90b042131370
marcoswlrich <marcosapw.dev@gmail.com>
2023-09-11 23:32:08 -0400

    Config Telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,119 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  cmd = 'Telescope',
+  version = false,
+  lazy = true,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'jvgrootveld/telescope-zoxide',
+    'nvim-tree/nvim-web-devicons',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-ui-select.nvim',
+    'telescope-dap.nvim',
+    'kkharji/sqlite.lua',
+    'nvim-telescope/telescope-frecency.nvim',
+    -- {
+    --   "nvim-telescope/telescope-file-browser.nvim",
+    --   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+    -- }
+  },
+  config = function()
+    -- import telescope plugin safely
+    local telescope_setup, telescope = pcall(require, "telescope")
+    if not telescope_setup then
+      return
+    end
+
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>fc", builtin.grep_string, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+    -- import telescope actions safely
+    local actions_setup, actions = pcall(require, "telescope.actions")
+    if not actions_setup then
+      return
+    end
+    require('nvim-web-devicons').setup({
+      override = {},
+      default = true
+    })
+
+    -- configure telescope
+    telescope.setup({
+      -- configure custom mappings
+      defaults = {
+        path_display = { 'smart' },
+        mappings = {
+          i = {
+            ["<C-u>"] = actions.preview_scrolling_up,
+            ["<C-d>"] = actions.preview_scrolling_down,
+            ["<esc>"] = actions.close
+          },
+        },
+        file_ignore_patterns = {
+          ".git/",
+          "target/",
+          "docs/",
+          "vendor/*",
+          "%.lock",
+          "__pycache__/*",
+          "%.sqlite3",
+          "%.ipynb",
+          "node_modules/*",
+          -- "%.jpg",
+          -- "%.jpeg",
+          -- "%.png",
+          "%.svg",
+          "%.otf",
+          "%.ttf",
+          "%.webp",
+          ".dart_tool/",
+          ".github/",
+          ".gradle/",
+          ".idea/",
+          ".settings/",
+          ".vscode/",
+          "__pycache__/",
+          "build/",
+          "env/",
+          "gradle/",
+          "node_modules/",
+          "%.pdb",
+          "%.dll",
+          "%.class",
+          "%.exe",
+          "%.cache",
+          "%.ico",
+          "%.pdf",
+          "%.dylib",
+          "%.jar",
+          "%.docx",
+          "%.met",
+          "smalljre_*/*",
+          ".vale/",
+          "%.burp",
+          "%.mp4",
+          "%.mkv",
+          "%.rar",
+          "%.zip",
+          "%.7z",
+          "%.tar",
+          "%.bz2",
+          "%.epub",
+          "%.flac",
+          "%.tar.gz",
+        },
+        prompt_prefix = "   ",
+      },
+      layout_config = {
+        horizontal = {
+          preview_cutoff = 100,
+          preview_width = 0.6
+        }
+      }
+    })
+  end,
+}

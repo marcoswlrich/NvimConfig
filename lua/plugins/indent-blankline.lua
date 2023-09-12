439df3bb8930a8bc2b25db1a354d6530931a2fc4
marcoswlrich <marcosapw.dev@gmail.com>
2023-09-12 00:00:08 -0400

    Config Indent

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  enabled = true,
+  opts = {
+    --char = "▏",
+    char = '┊',
+    -- char = "│",
+    filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+    show_end_of_line = true,
+    show_trailing_blankline_indent = false,
+    show_current_context = false,
+  },
+}

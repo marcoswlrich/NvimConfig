2bc287dd8d915163c319f14d7d2dc46d25e05be8
marcoswlrich <marcosapw.dev@gmail.com>
2023-09-13 11:33:05 -0400

    Update Config Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,13 +24,6 @@ return {
       return
     end
 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set("n", "<leader>fc", builtin.grep_string, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
     -- import telescope actions safely
     local actions_setup, actions = pcall(require, "telescope.actions")
     if not actions_setup then
@@ -109,11 +102,135 @@ return {
         prompt_prefix = "   ",
       },
       layout_config = {
-        horizontal = {
-          preview_cutoff = 100,
-          preview_width = 0.6
-        }
+        prompt_position = "top",
+        preview_cutoff = 120,
+      },
+      pickers = {
+        find_files = {
+          -- theme = "dropdown",
+          previewer = true,
+          layout_config = {
+            -- width = 0.5,
+            height = 0.8,
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
+        },
+        git_files = {
+          previewer = true,
+          -- theme = "dropdown",
+          layout_config = {
+            -- width = 0.5,
+            height = 0.8,
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
+        },
+        buffers = {
+          previewer = false,
+          -- theme = "dropdown",
+          layout_config = {
+            width = 0.5,
+            height = 0.4,
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
+        },
+        current_buffer_fuzzy_find = {
+          previewer = true,
+          -- theme = "dropdown",
+          layout_config = {
+            -- width = 0.5,
+            height = 0.8,
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
+        },
+        live_grep = {
+          only_sort_text = true,
+          previewer = true,
+          layout_config = {
+            horizontal = {
+              width = 0.9,
+              height = 0.75,
+              preview_width = 0.6,
+            },
+          },
+        },
+        grep_string = {
+          only_sort_text = true,
+          previewer = true,
+          layout_config = {
+            horizontal = {
+              width = 0.9,
+              height = 0.75,
+              preview_width = 0.6,
+            },
+          },
+        },
+        lsp_references = {
+          show_line = false,
+          previewer = true,
+          layout_config = {
+            horizontal = {
+              width = 0.9,
+              height = 0.75,
+              preview_width = 0.6,
+            },
+          },
+        },
+        treesitter = {
+          show_line = false,
+          previewer = true,
+          layout_config = {
+            horizontal = {
+              width = 0.9,
+              height = 0.75,
+              preview_width = 0.6,
+            },
+          },
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,                   -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+        },
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({
+            previewer        = false,
+            initial_mode     = "normal",
+            sorting_strategy = 'ascending',
+            layout_strategy  = 'horizontal',
+            layout_config    = {
+              horizontal = {
+                width = 0.5,
+                height = 0.4,
+                preview_width = 0.6,
+              },
+            },
+          })
+        },
+        frecency = {
+          default_workspace = 'CWD',
+          show_scores = true,
+          show_unindexed = true,
+          disable_devicons = false,
+          ignore_patterns = {
+            "*.git/*",
+            "*/tmp/*",
+            "*/lua-language-server/*",
+          },
+        },
       }
     })
+    telescope.load_extension('fzf')
+    telescope.load_extension('ui-select')
+    telescope.load_extension('refactoring')
+    telescope.load_extension('dap')
+    telescope.load_extension("zoxide")
+    telescope.load_extension("frecency")
   end,
 }

ceba78db7eb838666c4a9fd93edb59e96bc20d4c
marcoswlrich <marcosapw.dev@gmail.com>
2023-09-13 13:43:14 -0400

    Add file utils

---
+++ lua/core/utils.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.telescope_git_or_file = function()
+  local path = vim.fn.expand("%:p:h")
+  local git_dir = vim.fn.finddir(".git", path .. ";")
+  if #git_dir > 0 then
+    require("telescope.builtin").git_files()
+  else
+    require("telescope.builtin").find_files()
+  end
+end
+
+return M

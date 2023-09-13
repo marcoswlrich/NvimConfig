8549ba1d4d28d12a62695a2e8c866763a349bb15
marcoswlrich <marcosapw.dev@gmail.com>
2023-09-13 12:24:58 -0400

    Update config Treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -38,6 +38,7 @@ return {
         },
         context_commentstring = {
           enable = true,
+          enable_autocmd = false,
         },
 
         sync_install = false,
@@ -55,6 +56,68 @@ return {
         },
         autotag = {
           enable = true,
+        },
+
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<leader>vv",
+            node_incremental = "+",
+            scope_incremental = false,
+            node_decremental = "_",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = { query = "@function.outer", desc = "around a function" },
+              ["if"] = { query = "@function.inner", desc = "inner part of a function" },
+              ["ac"] = { query = "@class.outer", desc = "around a class" },
+              ["ic"] = { query = "@class.inner", desc = "inner part of a class" },
+              ["ai"] = { query = "@conditional.outer", desc = "around an if statement" },
+              ["ii"] = { query = "@conditional.inner", desc = "inner part of an if statement" },
+              ["al"] = { query = "@loop.outer", desc = "around a loop" },
+              ["il"] = { query = "@loop.inner", desc = "inner part of a loop" },
+              ["ap"] = { query = "@parameter.outer", desc = "around parameter" },
+              ["ip"] = { query = "@parameter.inner", desc = "inside a parameter" },
+            },
+            selection_modes = {
+              ["@parameter.outer"] = "v",   -- charwise
+              ["@parameter.inner"] = "v",   -- charwise
+              ["@function.outer"] = "v",    -- charwise
+              ["@conditional.outer"] = "V", -- linewise
+              ["@loop.outer"] = "V",        -- linewise
+              ["@class.outer"] = "<c-v>",   -- blockwise
+            },
+            include_surrounding_whitespace = false,
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_previous_start = {
+              ["[f"] = { query = "@function.outer", desc = "Previous function" },
+              ["[c"] = { query = "@class.outer", desc = "Previous class" },
+              ["[p"] = { query = "@parameter.inner", desc = "Previous parameter" },
+            },
+            goto_next_start = {
+              ["]f"] = { query = "@function.outer", desc = "Next function" },
+              ["]c"] = { query = "@class.outer", desc = "Next class" },
+              ["]p"] = { query = "@parameter.inner", desc = "Next parameter" },
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ["<leader>a"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<leader>A"] = "@parameter.inner",
+            },
+          },
         },
       })
 
@@ -74,11 +137,14 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
       require 'treesitter-context'.setup {
-        enable = true,         -- Enable this plugin (Can be enabled/disabled later via commands)
-        max_lines = 0,         -- How many lines the window should span. Values <= 0 mean no limit.
-        trim_scope = 'outer',  -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        patterns = {           -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        max_lines = 0,
+        line_numbers = true,
+        multiline_threshold = 20, -- How many lines the window should span. Values <= 0 mean no limit.
+        trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        min_window_height = 0,
+        on_attach = nil,
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
           -- For all filetypes
           -- Note that setting an entry here replaces all other patterns for this entry.
           -- By setting the 'default' entry below, you can control which nodes you want to

f2fa80460bd6016e108a6fb12aadaa81cbccbf21
marcoswlrich <marcosapw.dev@gmail.com>
2023-09-24 00:11:28 -0400

    Add snippets

--- lua/plugins/cmp/cmp.lua
+++ lua/plugins/cmp/cmp.lua
@@ -124,38 +124,9 @@ highlight! default link CmpItemKind CmpItemMenuDefault
       enable_autosnippets = true,
     })
 
-    local s = luasnip.snippet
-    local t = luasnip.text_node
-    local i = luasnip.insert_node
-    local d = luasnip.dynamic_node
-    local sn = luasnip.snippet_node
-
-    luasnip.add_snippets("typescriptreact", {
-      s("log", {
-        t("console.log("),
-        i(1, ""),
-        t(","),
-        i(2, ""),
-        t(")"),
-      }),
-      s("rc", {
-        t("export type "),
-        i(1),
-        t("Props = {}"),
-        t({ "", "export function " }),
-        d(2, function(args)
-          -- the returned snippetNode doesn't need a position; it's inserted
-          -- "inside" the dynamicNode.
-          return sn(nil, {
-            -- jump-indices are local to each snippetNode, so restart at 1.
-            i(1, args[1]),
-          })
-        end, { 1 }),
-        t("(){return null}"),
-      }),
-    })
-
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_lua").load({ paths = os.getenv("HOME") .. "/.config/nvim/snippets/" })
+
 
     lspkind.init({
       -- enables text annotations

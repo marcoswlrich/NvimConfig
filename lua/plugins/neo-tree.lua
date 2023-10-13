080079aca29d3634158804f82a2045a003250d11
marcoswlrich <marcosapw.dev@gmail.com>
2023-10-13 01:21:04 -0400

    Update neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,122 +1,141 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	event = "VeryLazy",
-	keys = {
-		{ "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
-		{ "<leader><tab>", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
-	},
-	config = function()
-		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    {
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      config = function()
+        require 'window-picker'.setup({
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', "quickfix" },
+            },
+          },
+        })
+      end,
+    },
+  },
+  event = "VeryLazy",
+  keys = {
+    { "<leader>e",     ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
+    { "<leader><tab>", ":Neotree toggle left<CR>",  silent = true, desc = "Left File Explorer" },
+  },
+  config = function()
+    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-		require("neo-tree").setup({
-			close_if_last_window = true,
-			popup_border_style = "rounded",
-			enable_git_status = true,
-			enable_modified_markers = true,
-			enable_diagnostics = true,
-			sort_case_insensitive = true,
-			default_component_configs = {
-				container = {
-					enable_character_fade = true,
-				},
-				indent = {
-					indent_size = 2,
-					padding = 1, -- extra padding on left hand side
-					indent_marker = "│",
-					last_indent_marker = "└",
-					highlight = "NeoTreeIndentMarker",
-					with_markers = true,
-					with_expanders = true,
-					expander_collapsed = "",
-					expander_expanded = "",
-					expander_highlight = "NeoTreeExpander",
-				},
-				modified = {
-					symbol = " ",
-					highlight = "NeoTreeModified",
-				},
-				icon = {
-					folder_closed = "",
-					folder_open = "",
-					folder_empty = "󰜌",
-					-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
-					-- then these will never be used.
-					default = "*",
-					highlight = "NeoTreeFileIcon",
-				},
-				git_status = {
-					symbols = {
-						-- Change type
-						added = "",
-						deleted = "",
-						modified = "",
-						renamed = "",
-						-- Status type
-						untracked = "",
-						ignored = "",
-						unstaged = "",
-						staged = "",
-						conflict = "",
-					},
-				},
-			},
-			window = {
-				position = "left",
-				width = 35,
-			},
-			filesystem = {
-				use_libuv_file_watcher = true,
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					hide_by_name = {
-						"node_modules",
-					},
-					never_show = {
-						".DS_Store",
-						"thumbs.db",
-					},
-				},
-			},
-			event_handlers = {
-				-- {
-				--     event = "file_opened",
-				--     handler = function()
-				--         --auto close
-				--         require("neo-tree").close_all()
-				--     end,
-				-- },
-				{
-					event = "neo_tree_window_after_open",
-					handler = function(args)
-						if args.position == "left" or args.position == "right" then
-							vim.cmd("wincmd =")
-						end
-					end,
-				},
-				{
-					event = "neo_tree_window_after_close",
-					handler = function(args)
-						if args.position == "left" or args.position == "right" then
-							vim.cmd("wincmd =")
-						end
-					end,
-				},
-			},
-			git_status = {
-				window = {
-					position = "float",
-				},
-			},
-		})
-	end,
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_modified_markers = true,
+      enable_diagnostics = true,
+      sort_case_insensitive = true,
+      default_component_configs = {
+        container = {
+          enable_character_fade = true,
+        },
+        indent = {
+          indent_size = 2,
+          padding = 1, -- extra padding on left hand side
+          indent_marker = "│",
+          last_indent_marker = "└",
+          highlight = "NeoTreeIndentMarker",
+          with_markers = true,
+          with_expanders = true,
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
+        },
+        modified = {
+          symbol = " ",
+          highlight = "NeoTreeModified",
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "󰜌",
+          -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+          -- then these will never be used.
+          default = "*",
+          highlight = "NeoTreeFileIcon",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "",
+            deleted = "",
+            modified = "",
+            renamed = "",
+            -- Status type
+            untracked = "",
+            ignored = "",
+            unstaged = "",
+            staged = "",
+            conflict = "",
+          },
+        },
+      },
+      window = {
+        position = "left",
+        width = 35,
+      },
+      filesystem = {
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+          },
+          never_show = {
+            ".DS_Store",
+            "thumbs.db",
+          },
+        },
+      },
+      event_handlers = {
+        -- {
+        --     event = "file_opened",
+        --     handler = function()
+        --         --auto close
+        --         require("neo-tree").close_all()
+        --     end,
+        -- },
+        {
+          event = "neo_tree_window_after_open",
+          handler = function(args)
+            if args.position == "left" or args.position == "right" then
+              vim.cmd("wincmd =")
+            end
+          end,
+        },
+        {
+          event = "neo_tree_window_after_close",
+          handler = function(args)
+            if args.position == "left" or args.position == "right" then
+              vim.cmd("wincmd =")
+            end
+          end,
+        },
+      },
+      git_status = {
+        window = {
+          position = "float",
+        },
+      },
+    })
+  end,
 }

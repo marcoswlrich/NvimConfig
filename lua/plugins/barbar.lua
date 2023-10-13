0264d4703215071354c32d093c75d0ba0c4e8720
marcoswlrich <marcosapw.dev@gmail.com>
2023-10-13 01:21:35 -0400

    Update Barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,47 +1,48 @@
 return {
-	"romgrk/barbar.nvim",
-	dependencies = {
-		"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-	},
-	init = function()
-		vim.g.barbar_auto_setup = false
-	end,
-	opts = {
-		animation = true,
-		tabpages = true,
-		clickable = true,
-		icons = {
-			-- Configure the base icons on the bufferline.
-			buffer_index = false,
-			buffer_number = false,
-			button = "",
-			-- Enables / disables diagnostic symbols
-			diagnostics = {
-				[vim.diagnostic.severity.ERROR] = { enabled = false, icon = "ﬀ" },
-				[vim.diagnostic.severity.WARN] = { enabled = false },
-				[vim.diagnostic.severity.INFO] = { enabled = false },
-				[vim.diagnostic.severity.HINT] = { enabled = false },
-			},
-			filetype = {
-				-- Sets the icon's highlight group.
-				-- If false, will use nvim-web-devicons colors
-				custom_colors = false,
-				-- Requires `nvim-web-devicons` if `true`
-				enabled = true,
-			},
-			separator = { left = "▎", right = "" },
-			-- Configure the icons on the bufferline when modified or pinned.
-			-- Supports all the base icon options.
-			modified = { button = "●" },
-			pinned = { button = "車", filename = true },
-			-- Configure the icons on the bufferline based on the visibility of a buffer.
-			-- Supports all the base icon options, plus `modified` and `pinned`.
-			alternate = { filetype = { enabled = false } },
-			current = { buffer_index = false },
-			inactive = { button = "×" },
-			visible = { modified = { buffer_number = false } },
-		},
-	},
-	version = "^1.0.0", -- optional: only update when a new 1.x version is released
+  "romgrk/barbar.nvim",
+  dependencies = {
+    "lewis6991/gitsigns.nvim",     -- OPTIONAL: for git status
+    "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {
+    animation = true,
+    tabpages = true,
+    clickable = true,
+    icons = {
+      -- Configure the base icons on the bufferline.
+      buffer_index = false,
+      buffer_number = false,
+      button = "",
+      -- Enables / disables diagnostic symbols
+      diagnostics = {
+        [vim.diagnostic.severity.ERROR] = { enabled = false, icon = "ﬀ" },
+        [vim.diagnostic.severity.WARN] = { enabled = false },
+        [vim.diagnostic.severity.INFO] = { enabled = false },
+        [vim.diagnostic.severity.HINT] = { enabled = false },
+      },
+      filetype = {
+        -- Sets the icon's highlight group.
+        -- If false, will use nvim-web-devicons colors
+        custom_colors = false,
+        -- Requires `nvim-web-devicons` if `true`
+        enabled = true,
+      },
+      separator = { left = "▎", right = "" },
+      separator_at_end = true,
+      -- Configure the icons on the bufferline when modified or pinned.
+      -- Supports all the base icon options.
+      modified = { button = " " },
+      pinned = { button = "車", filename = true },
+      -- Configure the icons on the bufferline based on the visibility of a buffer.
+      -- Supports all the base icon options, plus `modified` and `pinned`.
+      alternate = { filetype = { enabled = false } },
+      current = { buffer_index = false },
+      inactive = { button = "×" },
+      visible = { modified = { buffer_number = false } },
+    },
+  },
+  version = "^1.0.0", -- optional: only update when a new 1.x version is released
 }

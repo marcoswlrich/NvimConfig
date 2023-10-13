36c80a7018030a230d1d10dccb0b2bc425ea6734
marcoswlrich <marcosapw.dev@gmail.com>
2023-10-13 01:26:13 -0400

    Add Barbecue

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,12 @@
+return {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",
+  },
+  ---@type barbecue.Config
+  opts = {
+    theme = "catppuccin",
+  },
+}

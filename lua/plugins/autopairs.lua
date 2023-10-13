426075edcd908eb7cd82e2b2c3988ac549c342da
marcoswlrich <marcosapw.dev@gmail.com>
2023-10-13 01:26:44 -0400

    Update autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,16 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	opts = {
-		disable_filetype = { "TelescopePrompt", "vim" },
-	},
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = {
+    disable_filetype = { "TelescopePrompt", "vim" },
+    ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], '%s+', ''),
+    pairs_map = {
+      ["'"] = "'",
+      ['"'] = '"',
+      ['('] = ')',
+      ['['] = ']',
+      ['{'] = '}',
+      ['`'] = '`',
+    },
+  },
 }

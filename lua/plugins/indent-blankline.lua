2cfea538d5ad9d679d83e45d8043e896abec1419
marcoswlrich <marcosapw.dev@gmail.com>
2023-10-13 01:24:31 -0400

    Remove bug config

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,13 +2,19 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPost", "BufNewFile" },
   enabled = true,
-  opts = {
-    --char = "▏",
-    char = '┊',
-    -- char = "│",
-    filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-    show_end_of_line = true,
-    show_trailing_blankline_indent = false,
-    show_current_context = false,
-  },
+  main = "ibl",
+  opts = {},
+  config = function()
+    require("ibl").setup({
+      indent = {
+        char = '┊',
+      },
+      scope = {
+        enabled = false,
+        show_start = false,
+        show_end = false,
+      }
+    });
+  end
+
 }
